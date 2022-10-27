98c9952f7211913f81c4895e9e50fb44156235a5
Arlyn <arlyn.goodrich@gmail.com>
2022-10-27 16:09:32 -0500

    DEV-194 Create Session

--- Source/FeastOrFamine/Private/Core/FFGameInstance.cpp
+++ Source/FeastOrFamine/Private/Core/FFGameInstance.cpp
@@ -2,4 +2,83 @@
 
 
 #include "Core/FFGameInstance.h"
+#include "FeastOrFamine/FeastOrFamine.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
 
+UFFGameInstance::UFFGameInstance()
+{
+}
+
+void UFFGameInstance::StartGame(bool bShouldPlayOnline)
+{
+	UE_LOG(LogFFGameInstance, Log, TEXT("Starting New Game.  Should play Online = %s"), bShouldPlayOnline ? TEXT("true") : TEXT("false"))
+
+	if(bShouldPlayOnline)
+	{
+		CreateServer();
+	}
+	else
+	{
+		//Open Default Level
+		UGameplayStatics::OpenLevel(GetWorld(),"RockShoals");
+	}
+	
+}
+
+void UFFGameInstance::JoinGame()
+{
+}
+
+void UFFGameInstance::Init()
+{
+	Super::Init();
+
+	if(const IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get())
+	{
+		SessionInterface = Subsystem->GetSessionInterface();
+		if(SessionInterface.IsValid())
+		{
+			//Assign delegates from session interface
+			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this,&UFFGameInstance::OnCreateSessionsComplete);
+			SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this,&UFFGameInstance::OnFindSessionComplete);
+			SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this,&UFFGameInstance::OnJoinSessionComplete);
+		}
+	}
+}
+
+void UFFGameInstance::OnCreateSessionsComplete(FName SessionName, bool bSucceeded)
+{
+	if(bSucceeded)
+	{
+		GetWorld()->ServerTravel('/Game/FeastOrFamine/Maps/RockShoals?listen');
+	}
+	else
+	{
+		UE_LOG(LogFFGameInstance,Warning,TEXT("Failed to create online session"))
+	}
+}
+
+void UFFGameInstance::OnFindSessionComplete(bool bSucceeded)
+{
+}
+
+void UFFGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void UFFGameInstance::CreateServer() const
+{
+	UE_LOG(LogFFGameInstance,Log,TEXT("Creating online session listen server"))
+
+	FOnlineSessionSettings SessionSettings;
+	SessionSettings.bAllowJoinInProgress = true;
+	SessionSettings.bIsDedicated = false;
+	SessionSettings.bIsLANMatch = (IOnlineSubsystem::Get()->GetSubsystemName() != "NULL") ? false : true;
+	SessionSettings.bShouldAdvertise = true;
+	SessionSettings.bUsesPresence = true;
+	SessionSettings.NumPublicConnections = 5;
+
+	SessionInterface->CreateSession(0,FName("Test Session"),SessionSettings);
+	
+}
--- Source/FeastOrFamine/Public/Core/FFGameInstance.h
+++ Source/FeastOrFamine/Public/Core/FFGameInstance.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "OnlineSessionSettings.h"
 #include "FFGameInstance.generated.h"
 
 /**
@@ -13,5 +16,65 @@ UCLASS()
 class FEASTORFAMINE_API UFFGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+
+	UFFGameInstance();
+
+protected:
+
+	// ----------- Main Menu UI Commands ----------- //
+
+	/**
+	 * @brief Will load default level and optionally create online session if playing online
+	 * @param bShouldPlayOnline If the player should host a listen server for this session
+	 */
+	UFUNCTION(BlueprintCallable)
+	void StartGame(bool bShouldPlayOnline);
+
+	/**
+	 * @brief If the player would like to join a game in progress
+	 */
+	UFUNCTION(BlueprintCallable)
+	void JoinGame();
 	
+
+	// ----------- Online SubSystem Code ----------- //
+	
+	/**
+	 * @brief Stores results from Session Search 
+	 */
+	TSharedPtr<FOnlineSessionSearch> SessionSearch;
+	
+	/**
+	 * @brief Online Session Interface pointer. Allows to create sessions, search for sessions, and join sessions. 
+	 */
+	IOnlineSessionPtr SessionInterface;
+	
+	virtual void Init() override;
+
+	/**
+	 * @brief Called by delegate from the Session Interface when a session is created
+	 * @param SessionName Name of the session that was created
+	 * @param bSucceeded If the session was successfully created 
+	 */
+	virtual void OnCreateSessionsComplete(FName SessionName, bool bSucceeded);
+
+	/**
+	 * @brief Called by delegate from the Session Interface when find sessions is complete
+	 * @param bSucceeded If the interface succeeded in finding online sessions 
+	 */
+	virtual void OnFindSessionComplete(bool bSucceeded);
+
+	/**
+	 * @brief Called by delegate from interface when Session is joined
+	 * @param SessionName Name of the session Joined
+	 * @param Result Success or failure of join
+	 */
+	virtual void OnJoinSessionComplete(FName SessionName,EOnJoinSessionCompleteResult::Type Result);
+	
+	/**
+	 * @brief Called by Start Game if bShouldPlayOnline = true.  Creates online session as listen server. 
+	 */
+	void CreateServer() const;
 };
