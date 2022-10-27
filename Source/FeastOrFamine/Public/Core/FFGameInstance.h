6d3d40a6e73deeb1d58b0fce389e39040c605baa
Arlyn <arlyn.goodrich@gmail.com>
2022-10-27 15:27:45 -0500

    DEV-194 Added Custom Game Instance Base Class

---
+++ Source/FeastOrFamine/Public/Core/FFGameInstance.h
@@ -0,0 +1,17 @@
+// 2022 Arlyn Goodrich All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "FFGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FEASTORFAMINE_API UFFGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/FeastOrFamine/Public/Core/FFGameModeBase.h
@@ -0,0 +1,17 @@
+// 2022 Arlyn Goodrich All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FFGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FEASTORFAMINE_API AFFGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
