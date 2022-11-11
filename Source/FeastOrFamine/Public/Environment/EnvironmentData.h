3b74c8df1c92448c9a12e743a0b7dc297effb77c
Arlyn <arlyn.goodrich@gmail.com>
2022-11-11 10:55:20 -0600

    DEV-204 FoliageISMC Map data class

--- Source/FeastOrFamine/Public/Environment/EnvironmentData.h
+++ Source/FeastOrFamine/Public/Environment/EnvironmentData.h
@@ -3,9 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "CustomFoliageBase.h"
 #include "ItemData.h"
 #include "EnvironmentData.Generated.h"
 
+class ACustomFoliageBase;
 
 /**
  * @brief Items given when a foliage actor is harvested
@@ -89,3 +91,72 @@ struct FHarvestReward
 			);
 	}
 };
+
+
+/**
+ * @brief Items given when a foliage actor is harvested
+ */
+USTRUCT(BlueprintType)
+struct FFoliageActorMeshMap 
+{
+	GENERATED_USTRUCT_BODY()
+
+	/**
+	 * @brief Foliage ISMC Static Mesh 
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "FoliageMap")
+	UStaticMesh* FISMCStaticMesh;
+
+	/**
+	 * @brief Actor To Spawn to replace Foliage ISMC
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "FoliageMap")
+	TSubclassOf<ACustomFoliageBase> FoliageActorToSpawn;
+
+
+	/**
+	 * @brief Is valid flag, null with default constructor
+	 */
+	bool bIsValid; 
+
+	
+	/**
+	 * @brief Default constructor will be invalid
+	 */
+	FFoliageActorMeshMap()
+	{
+		FISMCStaticMesh = nullptr;
+		FoliageActorToSpawn = nullptr;
+		bIsValid = false;
+	}
+
+	/**
+	 * @brief Default constructor that will be valid if both StaticMesh and Foliage Actor Classes are valid
+	 */
+	FFoliageActorMeshMap(UStaticMesh* SetFoliageISMCMesh, const TSubclassOf<ACustomFoliageBase> SetFoliageActorToSpawn)
+	{
+		FISMCStaticMesh = SetFoliageISMCMesh;
+		FoliageActorToSpawn = SetFoliageActorToSpawn;
+
+		
+		bIsValid = (SetFoliageISMCMesh != nullptr && SetFoliageActorToSpawn != nullptr);
+	}
+
+	/**
+	 * @brief The structs are the same if:
+	 * The reward item is the same
+	 * The default amount given is the same
+	 * Both items are valid
+	 */
+	bool operator==(const FFoliageActorMeshMap& FoliageActorMeshMap) const
+	{
+		return
+			(
+				FoliageActorMeshMap.FISMCStaticMesh == FISMCStaticMesh &&
+				FoliageActorMeshMap.FoliageActorToSpawn == FoliageActorToSpawn &&
+				FoliageActorMeshMap.bIsValid &&
+				bIsValid
+			);
+	}
+};
+
